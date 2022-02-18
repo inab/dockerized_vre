3fdf43b2f36b8756f4279169694d251266571637
root <root@eucanimage-dev.vm.bsc.es>
2022-02-18 18:29:42 +0100

    .

---
+++ config/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+// set up app settings
+require "/var/www/html/openVRE/config/globals.inc.php";
+
+// import vendor libs
+require dirname(__FILE__)."/vendor/autoload.php"; 
+
+// initialize session
+require "/var/www/html/openVRE/public/phplib/session.inc";
+
+// import local classes
+foreach(glob("/var/www/html/openVRE/public/phplib/classes/*.php") as $lib){
+    require $lib;
+}
+// import local libs
+foreach(glob("/var/www/html/openVRE/public/phplib/*.php") as $lib){
+    require $lib;
+}
+
+?>
