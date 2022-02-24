0752b2b33a4828d5393ce6595765919b03b28fd9
root <root@eucanimage-dev.vm.bsc.es>
2022-02-24 14:26:25 +0100

    .

--- mongodb/mongo-init.js
+++ mongodb/mongo-init.js
@@ -5,7 +5,7 @@ db.createUser(
             roles: [
                 {
                     role: "readWrite",
-                    db: "vre-db"
+                    db: "open-vre"
                 }
             ]
         }
